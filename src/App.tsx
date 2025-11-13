182fa75115d2100c5217bdfbc47532611858ee25
Matías Battocchia <matias@riseup.net>
2025-11-13 15:35:10 -0300

    grid css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 function App() {
-  return (
-    <div className="flex h-[100dvh] w-full bg-background">
-      {/* Menu (fixed width) */}
+  // TODO: Toggle this state when user opens contact details panel
+  const showRightPanel = false;
 
-      {/* Panel */}
-      <div className="flex grow max-w-[100%] lg:max-w-[calc(100%-65px)]">
-        Hola
+  return (
+    <div className={`app-grid ${showRightPanel ? "with-right-panel" : ""}`}>
+      {/* Left Menu Column - Fixed 64px */}
+      <div className="bg-sidebar border-r border-sidebar-border">
+        {/* Menu component goes here */}
+        Menu
       </div>
+
+      {/* Chat List Panel - 300-400px */}
+      <div className="bg-background border-r border-border overflow-hidden">
+        {/* Chat list content goes here */}
+        Chat List
+      </div>
+
+      {/* Main Chat Panel - Takes remaining space */}
+      <div className="bg-chat overflow-hidden">
+        {/* Chat content goes here */}
+        Main Chat
+      </div>
+
+      {/* Right Panel (Contact Details) - Conditional */}
+      {showRightPanel && (
+        <div className="bg-background border-l border-border overflow-hidden">
+          {/* Contact details go here */}
+          Contact Details
+        </div>
+      )}
     </div>
   );
 }
