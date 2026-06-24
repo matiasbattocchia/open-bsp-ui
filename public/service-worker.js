a681b2da70c767b20033ba0fa568711d056e2729
Matías Battocchia <matias@riseup.net>
2026-06-24 09:42:39 -0300

    chore: update PWA manifest and branding assets while removing obsolete service worker

--- public/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Installing...");
-});
-
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(fetch(event.request)); // Forward all requests to the network
-// });
